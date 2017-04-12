597ecd062d32496679889bd9c524ed0cac52fcd5
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-04-12 15:56:09 -0600

    completed random

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var numberOfSquares = 81;
 for (var i = 0; i < numberOfSquares; i++) {
     var div = document.createElement('div');
-    div.style.backgroundColor = 'black';
+    var random1 = Math.floor(Math.random() * 255);
+    var random2 = Math.floor(Math.random() * 255);
+    var random3 = Math.floor(Math.random() * 255);
+
+    div.style.backgroundColor = 'rgb(' + random1 + ',' + random2 + ',' + random3 + ')';
     div.style.width = '11.1%';
     div.style.cssFloat = 'left';
     div.style.paddingBottom = '11.1%';
-    if (i % 2 !== 0) {
-      div.style.backgroundColor = 'red';
-    }
     document.body.appendChild(div);
 }
+
+// from 0 to 255
+// rgb(209, 198, 35)
+//rgb(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), )
+// Math.floor(Math.random() * 255)
