156b95419f8ea73eed8665a3f6e9d6053cb66a64
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-04-12 17:25:30 -0600

    changed gradient

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (var i = 0; i < numberOfSquares; i++) {
     div.style.cssFloat = 'left';
     div.style.paddingBottom = '11.1%';
     if (i % 2 !== 0) {
-      div.style.backgroundColor = 'rgb(0, 0, ' + gradient + ')';
+      div.style.backgroundColor = 'rgb(255, 0, ' + gradient + ')';
     } else {
       div.style.backgroundColor = 'rgb(' + gradient + ', 0, 0)';
     }
