3260d910b9f81d7c9d3ef237f01856f8752940f3
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-04-12 15:43:23 -0600

    Completed part 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JS goes here
+var numberOfSquares = 81;
+for (var i = 0; i < numberOfSquares; i++) {
+    var div = document.createElement('div');
+    div.style.backgroundColor = 'black';
+    div.style.width = '11.1%';
+    div.style.cssFloat = 'left';
+    div.style.paddingBottom = '11.1%';
+    if (i % 2 !== 0) {
+      div.style.backgroundColor = 'red';
+    }
+    document.body.appendChild(div);
+}
