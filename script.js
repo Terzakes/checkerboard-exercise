c1db0e3a996b3eb3a4140e6d592286c1ed508b2a
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-04-12 16:12:34 -0600

    completed gradient

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var numberOfSquares = 81;
+var gradient = 255;
+
 for (var i = 0; i < numberOfSquares; i++) {
     var div = document.createElement('div');
-    div.style.backgroundColor = 'black';
     div.style.width = '11.1%';
     div.style.cssFloat = 'left';
     div.style.paddingBottom = '11.1%';
     if (i % 2 !== 0) {
-      div.style.backgroundColor = 'red';
+      div.style.backgroundColor = 'rgb(0, 0, ' + gradient + ')';
+    } else {
+      div.style.backgroundColor = 'rgb(' + gradient + ', 0, 0)';
     }
+    gradient -= 2;
     document.body.appendChild(div);
 }
