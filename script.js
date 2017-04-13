a2fc51e2d61960fe753d2a40a5aa07b6ce8339d4
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-04-12 18:53:51 -0600

    finished flashing

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-var numberOfSquares = 81;
-for (var i = 0; i < numberOfSquares; i++) {
-    var div = document.createElement('div');
-    var random1 = Math.floor(Math.random() * 255);
-    var random2 = Math.floor(Math.random() * 255);
-    var random3 = Math.floor(Math.random() * 255);
 
-    div.style.backgroundColor = 'rgb(' + random1 + ',' + random2 + ',' + random3 + ')';
-    div.style.width = '11.1%';
-    div.style.cssFloat = 'left';
-    div.style.paddingBottom = '11.1%';
-    document.body.appendChild(div);
+// .style.animationDuration = '2s';
+// .style.animationDelay = '2s';
+// .style.animationIterationCount = 'infinite';
+function makeBoard () {
+  var numberOfSquares = 81;
+
+  for (var i = 0; i < numberOfSquares; i++) {
+      var div = document.createElement('div');
+      var color;
+      var random1 = Math.floor(Math.random() * 255);
+      var random2 = Math.floor(Math.random() * 255);
+      var random3 = Math.floor(Math.random() * 255);
+
+      div.style.backgroundColor = 'rgb(' + random1 + ',' + random2 + ',' + random3 + ')';
+      div.style.width = '11.1%';
+      div.style.cssFloat = 'left';
+      div.style.paddingBottom = '11.1%';
+
+      document.body.appendChild(div);
+
+  }
+
 }
-
-// from 0 to 255
-// rgb(209, 198, 35)
-//rgb(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), )
-// Math.floor(Math.random() * 255)
+function hideAll () {
+  document.body.innerHTML = '';
+}
+window.setInterval(makeBoard, 2000);
+window.setInterval(hideAll, 1999);
